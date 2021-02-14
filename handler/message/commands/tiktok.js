ed7a5cc7ab7240b85161364c3746470ee64ee9bf
Tarcisio Caetano <tarcisiocaetano2009@gmail.com>
2021-02-14 14:22:29 -0300

    Complete rework on TikTok downloader command

--- handler/message/commands/tiktok.js
+++ handler/message/commands/tiktok.js
@@ -1,10 +1,11 @@
 const {
-    downloader
-} = require('../../../lib');
-const {
     isUrl,
     color
 } = require('../../../utils')
+const TikTokScraper = require("tiktok-scraper");
+
+const fullLinkRegex = /https:\/\/www\.tiktok\.com\/@.*\/video\/\d{18,}.*/;
+const shortLinkRegex = /https??:\/\/(v[m|t]\.)??tiktok\.com\/(\w|\W|\d)+/;
 
 const tikTokCommand = async (client, message, args) => {
     const {
@@ -14,16 +15,23 @@ const tikTokCommand = async (client, message, args) => {
     const url = args.length !== 0 ? args[0] : ''
 
     if (args.length !== 1) return client.reply(from, 'O formato da mensagem está errado, verifique o correto no menu!', id)
-    if (!isUrl(url) && !url.includes('tiktok.com')) return client.reply(from, 'Desculpe, o link que você enviou é inválido.', id)
+    if (!isUrl(url) && !fullLinkRegex.test(url) & !shortLinkRegex(url)) return client.reply(from, 'Desculpe, o link que você enviou é inválido.', id)
     await client.reply(from, '_Pegando dados..._', id)
 
-    downloader.tiktok(url)
-        .then(async (res) => {
-            return await client.sendFileFromUrl(from, res, 'videoTikTok.mp4', '', id)
+    TikTokScraper.getVideoMeta(url, {
+            noWaterMark: true,
+            hdVideo: true,
         })
-        .catch(err => {
+        .then((res) => {
+            const headers = {
+                headers: res.headers
+            }
+            res = res.collector[0]
+
+            return client.sendFileFromUrl(from, res.videoUrl, 'tiktokVideo.mp4', res.text, id, headers)
+        })
+        .catch((err) => {
             console.log(color('[ERROR TIKTOK]', 'red'), err.message)
-            if (err.message.includes('TIMEDOUT')) return client.reply(from, 'Tente denovo, o servidor tá meio lento', id)
         })
 }
 
